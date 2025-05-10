982bbdc551b8bddf4ff7a50f7512caf7eed1f38d
Gonzalo Vivanco Zepeda <class.nonetype@gmail.com>
2025-05-09 20:32:59 -0400

    ...

--- src/main/java/com/jclient/models/interfaces/ViewInterface.java
+++ src/main/java/com/jclient/models/interfaces/ViewInterface.java
@@ -6,6 +6,7 @@ import java.awt.*;
 
 public interface ViewInterface {
 
+    // Ancho x Alto
     int signInWindowWidth = 680;
     int signInWindowHeight = 400;
 
@@ -15,24 +16,27 @@ public interface ViewInterface {
     int clientFormWindowWidth = 0;
     int clientFormWindowHeight = 0;
 
+    // Color
     Color primaryBackgroundColor = new Color(250, 250, 250);
     Color secondaryBackgroundColor = new Color(192, 192, 192);
 
 
     Color textFieldBorderColor = new Color(36, 114, 200);
     Color textFieldFontColor = new Color(36, 114, 200);
-    Color labelTextColor = new Color(0, 0, 0);
+    Color primaryLabelTextColor = new Color(0, 0, 0);
 
     Color primaryButtonBaseColor = new Color(60, 60, 60);
     Color primaryButtonHoverColor = new Color(90, 90, 90);
 
 
-    // Bordes y fuente
+    // Borde
     MatteBorder textFieldBorder = BorderFactory.createMatteBorder(
             0, 0, 2, 0, textFieldBorderColor
     );
+
+    // Fuente
     Font textFieldFont = new Font("Segoe UI", Font.PLAIN, 12);
-    Font labelTextFont = new Font("Segoe UI", Font.PLAIN, 15);
+    Font primaryLabelTextFont = new Font("Segoe UI", Font.PLAIN, 15);
 
 
 }
